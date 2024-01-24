6c81213329f99a90d90f4f5f61a49bb9fe3b255e
quingsley <jeromejuma25@gmail.com>
2024-01-24 06:31:47 +0300

    Add WidgetsFlutterBinding.ensureInitialized() to main() function

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:carbon_zero/services/local_storage.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await LocalStorage().init();
   runApp(const CarbonZero());
 }
