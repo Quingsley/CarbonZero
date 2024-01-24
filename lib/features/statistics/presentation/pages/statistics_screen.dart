7535515faf06021db4d9c78971936f4a7d221873
quingsley <jeromejuma25@gmail.com>
2024-01-24 06:34:01 +0300

    Add statistics cards to StatisticsScreen

--- lib/features/statistics/presentation/pages/statistics_screen.dart
+++ lib/features/statistics/presentation/pages/statistics_screen.dart
@@ -1,12 +1,75 @@
+import 'package:carbon_zero/features/statistics/presentation/widgets/statistcs_card.dart';
 import 'package:flutter/material.dart';
 
+/// will show the users statistics of their carbon footprint
 class StatisticsScreen extends StatelessWidget {
+  /// constructor call
   const StatisticsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Statistics Screen')),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Statistics'),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text.rich(
+              TextSpan(
+                text: 'Average Footprint: ',
+                children: [
+                  TextSpan(
+                    text: '18979kg',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: StatisticsCard(
+                    type: 'Land',
+                    averageFootPrint: '150',
+                    icon: Icons.landscape,
+                  ),
+                ),
+                Expanded(
+                  child: StatisticsCard(
+                    type: 'Water',
+                    averageFootPrint: '100',
+                    icon: Icons.waves,
+                  ),
+                ),
+                Expanded(
+                  child: StatisticsCard(
+                    type: 'Energy',
+                    averageFootPrint: '15',
+                    icon: Icons.flash_on_outlined,
+                  ),
+                ),
+                Expanded(
+                  child: StatisticsCard(
+                    type: 'Co2',
+                    averageFootPrint: '209',
+                    icon: Icons.cloud_outlined,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
