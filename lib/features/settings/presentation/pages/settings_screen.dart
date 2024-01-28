6af601122d3d0fb33941d881a54374db8899bd9f
quingsley <jeromejuma25@gmail.com>
2024-01-28 14:41:10 +0300

    add settings screen

--- lib/features/settings/presentation/pages/settings_screen.dart
+++ lib/features/settings/presentation/pages/settings_screen.dart
@@ -1,12 +1,81 @@
+import 'package:carbon_zero/core/extensions.dart';
+import 'package:carbon_zero/features/settings/presentation/widgets/setting_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 
+/// will have the user settings
 class SettingsScreen extends StatelessWidget {
+  /// constructor call
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Settings Screen')),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: ListView(
+          children: [
+            SettingsTile(
+              title: 'Profile',
+              onTap: () => context.push('/profile'),
+              icon: const Icon(Icons.person),
+              iconColor: context.colors.primary,
+            ),
+            SettingsTile(
+              title: 'Notifications',
+              onTap: () => context.push('/notification'),
+              icon: const Icon(Icons.notifications),
+              iconColor: context.colors.primary,
+            ),
+            SettingsTile(
+              title: 'App Theme',
+              onTap: () {},
+              icon: const Icon(Icons.dark_mode),
+              iconColor: context.colors.primary,
+            ),
+            SettingsTile(
+              title: 'Privacy',
+              onTap: () {},
+              icon: const Icon(Icons.privacy_tip),
+              iconColor: context.colors.primary,
+            ),
+            const Divider(
+              thickness: 2,
+            ),
+            SettingsTile(
+              title: 'Community',
+              onTap: () {},
+              icon: SvgPicture.asset(
+                'assets/images/community-icon.svg',
+                width: 24,
+                colorFilter: ColorFilter.mode(
+                  context.colors.primary,
+                  BlendMode.srcIn,
+                ),
+              ),
+              iconColor: context.colors.primary,
+            ),
+            SettingsTile(
+              title: 'FAQs',
+              onTap: () {},
+              icon: const Icon(Icons.message),
+              iconColor: context.colors.primary,
+            ),
+            SettingsTile(
+              title: 'Log out',
+              onTap: () {},
+              icon: const Icon(Icons.logout_rounded),
+              iconColor: context.colors.error,
+              fillColor: context.colors.errorContainer,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/features/settings/presentation/widgets/setting_tile.dart
@@ -0,0 +1,65 @@
+import 'package:carbon_zero/core/extensions.dart';
+import 'package:flutter/material.dart';
+
+/// settings tile
+class SettingsTile extends StatelessWidget {
+  /// constructor call
+  const SettingsTile({
+    required this.title,
+    required this.onTap,
+    required this.icon,
+    required this.iconColor,
+    super.key,
+    this.fillColor,
+  });
+
+  /// title
+  final String title;
+
+  /// on tap
+  final VoidCallback onTap;
+
+  /// icon
+  final Widget icon;
+
+  /// icon color
+  final Color iconColor;
+
+  /// fill color
+  final Color? fillColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: ListTile(
+        onTap: onTap,
+        contentPadding: EdgeInsets.zero,
+        leading: IconButton.filledTonal(
+          style: ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll<Color?>(fillColor),
+            shape: MaterialStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+          ),
+          onPressed: onTap,
+          color: iconColor,
+          icon: icon,
+        ),
+        title: Text(
+          title,
+          style: context.textTheme.titleMedium?.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        trailing: Icon(
+          Icons.arrow_forward_ios,
+          size: 16,
+          color: Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
