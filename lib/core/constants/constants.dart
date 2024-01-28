e7ab342cf9a7c472956fe352a2ee03493414bff9
quingsley <jeromejuma25@gmail.com>
2024-01-28 10:05:39 +0300

    Add sampleText constant

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -15,3 +15,6 @@ enum AppTabs {
   /// 4
   settings
 }
+
+const sampleText =
+    "We are a group of like-minded individuals who are passionate about creating a cleaner and more sustainable future. Whether you're interested in solar, wind , hydro, geothermal, or other forms of renewable energy, our community is the perfect place to learn, hare ideas, and collaborate on projects. From DIY solar panels to community and wind turbines, we believe in the power of renewable energy to transform our world. Join us today and become a part of the solution!";
