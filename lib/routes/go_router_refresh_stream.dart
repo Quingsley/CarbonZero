d7e2ccf0e2a40a89cc8bbe6e75870cb52b5a997a
quingsley <jeromejuma25@gmail.com>
2024-02-03 10:56:10 +0300

    Remove unused import in GoRouterRefreshStream

--- lib/routes/go_router_refresh_stream.dart
+++ lib/routes/go_router_refresh_stream.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 
-/// This class was imported from the migration guide for GoRouter 5.0
 class GoRouterRefreshStream extends ChangeNotifier {
   /// Constructor
   GoRouterRefreshStream(Stream<dynamic> stream) {
