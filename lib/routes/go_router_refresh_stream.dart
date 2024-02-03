7d25e9ffa289a1edac460dba4ca0974312d39999
quingsley <jeromejuma25@gmail.com>
2024-02-03 10:55:13 +0300

    Add GoRouterRefreshStream class

---
+++ lib/routes/go_router_refresh_stream.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+
+/// This class was imported from the migration guide for GoRouter 5.0
+class GoRouterRefreshStream extends ChangeNotifier {
+  /// Constructor
+  GoRouterRefreshStream(Stream<dynamic> stream) {
+    notifyListeners();
+    _subscription = stream.asBroadcastStream().listen(
+          (dynamic _) => notifyListeners(),
+        );
+  }
+
+  late final StreamSubscription<dynamic> _subscription;
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
+}
