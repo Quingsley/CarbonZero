32a90537b516441df33857607e13c1d7c6cd1e3c
quingsley <jeromejuma25@gmail.com>
2024-02-03 10:47:30 +0300

    Add Firebase initialization and update router usage

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,38 @@
 import 'package:carbon_zero/core/theme/theme.dart';
+
+import 'package:carbon_zero/firebase_options.dart';
 import 'package:carbon_zero/routes/app_routes.dart';
 import 'package:carbon_zero/services/local_storage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await LocalStorage().init();
-  runApp(const CarbonZero());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(child: CarbonZero()));
 }
 
 /// Root widget of the [CarbonZero] app.
-class CarbonZero extends StatelessWidget {
+class CarbonZero extends ConsumerWidget {
   /// Create const instance of app root widget.
   const CarbonZero({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(AppRoutes.router);
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'CarbonZero',
       darkTheme: darkTheme,
       theme: lightTheme,
       // themeMode: ThemeMode.dark,
-      routeInformationParser: AppRoutes.router.routeInformationParser,
-      routeInformationProvider: AppRoutes.router.routeInformationProvider,
-      routerDelegate: AppRoutes.router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
+      routerDelegate: router.routerDelegate,
     );
   }
 }
