96cffcb6aa92520976498e0ef9246d02a32e9cc2
quingsley <jeromejuma25@gmail.com>
2024-02-04 06:44:22 +0300

    Refactor ProfilePhoto to ProfilePhotoScreen and extract ProfilePhotoCard

--- lib/features/auth/presentation/pages/profile_photo.dart
+++ lib/features/auth/presentation/pages/profile_photo.dart
@@ -1,33 +1,12 @@
-import 'dart:io';
-
 import 'package:carbon_zero/core/widgets/primary_button.dart';
+import 'package:carbon_zero/core/widgets/profile_photo_card.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:image_picker/image_picker.dart';
 
-/// [ProfilePhoto] screen that allows user to set profile photo
-class ProfilePhoto extends StatefulWidget {
+/// [ProfilePhotoScreen] screen that allows user to set profile photo
+class ProfilePhotoScreen extends StatelessWidget {
   ///constructor call
-  const ProfilePhoto({super.key});
-
-  @override
-  State<ProfilePhoto> createState() => _ProfilePhotoState();
-}
-
-class _ProfilePhotoState extends State<ProfilePhoto> {
-  //TODO: refactor this into its own service
-  final ImagePicker picker = ImagePicker();
-  XFile? pickedImage;
-  String? imagePath;
-// Pick an image.
-
-  Future<void> pickImage() async {
-    // will need to show
-    pickedImage = await picker.pickImage(source: ImageSource.gallery);
-    setState(() {
-      imagePath = pickedImage?.path;
-    });
-  }
+  const ProfilePhotoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -56,54 +35,13 @@ class _ProfilePhotoState extends State<ProfilePhoto> {
             const SizedBox(
               height: 50,
             ),
-            Align(
-              child: Stack(
-                clipBehavior: Clip.none,
-                alignment: Alignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 100,
-                    child: imagePath != null
-                        ? Container(
-                            // height: 100,
-                            width: 150,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              // borderRadius: BorderRadius.circular(1000),
-                              image: DecorationImage(
-                                fit: BoxFit.contain,
-                                image: FileImage(
-                                  File(
-                                    imagePath!,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          )
-                        : const Icon(
-                            Icons.account_circle,
-                            size: 120,
-                          ),
-                  ),
-                  Positioned(
-                    bottom: 10,
-                    left: 140,
-                    child: IconButton.filled(
-                      onPressed: pickImage,
-                      icon: const Icon(Icons.camera_alt),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            const ProfilePhotoCard(),
             const Spacer(),
             PrimaryButton(
               text: 'Continue',
-              onPressed: imagePath != null
-                  ? () {
-                      context.go('/auth/profile-complete');
-                    }
-                  : null,
+              onPressed: () {
+                context.go('/auth/profile-complete');
+              },
             ),
             const SizedBox(
               height: 20,
