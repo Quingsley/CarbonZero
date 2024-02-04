83a6efcb5538926e719b7aef38379ab3aa06be74
quingsley <jeromejuma25@gmail.com>
2024-02-04 06:38:11 +0300

    Update constants.dart with sample text and add ImageType enum

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -16,5 +16,15 @@ enum AppTabs {
   settings
 }
 
+/// dummy text
 const sampleText =
-    "We are a group of like-minded individuals who are passionate about creating a cleaner and more sustainable future. Whether you're interested in solar, wind , hydro, geothermal, or other forms of renewable energy, our community is the perfect place to learn, hare ideas, and collaborate on projects. From DIY solar panels to community and wind turbines, we believe in the power of renewable energy to transform our world. Join us today and become a part of the solution!";
+    """ We are a group of like-minded individuals who are passionate about creating a cleaner and more sustainable future. Whether you're interested in solar, wind , hydro, geothermal, or other forms of renewable energy, our community is the perfect place to learn, hare ideas, and collaborate on projects. From DIY solar panels to community and wind turbines, we believe in the power of renewable energy to transform our world. Join us today and become a part of the solution!""";
+
+/// enum to specify the type of image
+enum ImageType {
+  /// profile image
+  profile,
+
+  /// community image
+  community
+}
