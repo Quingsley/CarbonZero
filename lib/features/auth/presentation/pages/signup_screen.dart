db1a60773a6a3e683d4508f3d1ceed31697f57e9
quingsley <jeromejuma25@gmail.com>
2024-02-04 06:45:03 +0300

    Refactor code to improve performance and readability

--- lib/features/auth/presentation/pages/signup_screen.dart
+++ lib/features/auth/presentation/pages/signup_screen.dart
@@ -1,4 +1,5 @@
 import 'package:carbon_zero/core/error/failure.dart';
+import 'package:carbon_zero/core/widgets/form_layout.dart';
 import 'package:carbon_zero/core/widgets/primary_button.dart';
 import 'package:carbon_zero/features/auth/data/models/user_model.dart';
 import 'package:carbon_zero/features/auth/presentation/viewmodels/auth_view_model.dart';
@@ -74,156 +75,127 @@ class _SignUpScreenState extends ConsumerState<SignUpScreen> {
     });
     return Scaffold(
       appBar: AppBar(),
-      body: LayoutBuilder(
-        builder: (context, viewportConstraints) {
-          return SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: viewportConstraints.maxHeight,
-              ),
-              child: IntrinsicHeight(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Create Account',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headlineMedium
-                              ?.copyWith(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 24,
-                              ),
-                        ),
-                        Text(
-                          'Join th green revolution!',
-                          style: Theme.of(context).textTheme.labelLarge,
-                          textAlign: TextAlign.left,
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        KTextField(
-                          label: 'First Name',
-                          hintText: 'First Name',
-                          controller: fNameController,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter your first name';
-                            }
-                            return null;
-                          },
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        KTextField(
-                          controller: lNameController,
-                          label: 'Last Name',
-                          hintText: 'Last Name',
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter your last name';
-                            }
-                            return null;
-                          },
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        KTextField(
-                          controller: emailController,
-                          label: 'Email',
-                          hintText: 'Email Address',
-                          validator: (value) {
-                            if (value == null ||
-                                value.isEmpty ||
-                                !value.contains('@')) {
-                              return 'Please enter your email';
-                            }
-                            return null;
-                          },
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        // will only use country hence to change to drop down menu
-
-                        KTextField(
-                          controller: passwordController,
-                          label: 'Password',
-                          hintText: 'Enter Password',
-                          isObscured: true,
-                          validator: (value) {
-                            if (value == null ||
-                                value.isEmpty ||
-                                value.length < 8) {
-                              return 'Please enter your password';
-                            }
-                            return null;
-                          },
-                        ),
-                        const SizedBox(
-                          height: 50,
-                        ),
-
-                        PrimaryButton(
-                          text: 'Create Account',
-                          onPressed: !isLoading ? submit : null,
-                          isLoading: isLoading,
-                        ),
-
-                        const SizedBox(
-                          height: 50,
-                        ),
-                        RichText(
-                          textAlign: TextAlign.center,
-                          text: TextSpan(
-                            text: 'By Creating an account you agree tou our ',
-                            style: Theme.of(context).textTheme.bodyMedium,
-                            children: [
-                              TextSpan(
-                                text: 'Terms of Service',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium
-                                    ?.copyWith(
-                                      color:
-                                          Theme.of(context).colorScheme.primary,
-                                      decoration: TextDecoration.underline,
-                                      decorationColor:
-                                          Theme.of(context).colorScheme.primary,
-                                    ),
-                              ),
-                              const TextSpan(text: ' and '),
-                              TextSpan(
-                                text: 'Privacy Policy',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium
-                                    ?.copyWith(
-                                      color:
-                                          Theme.of(context).colorScheme.primary,
-                                      decoration: TextDecoration.underline,
-                                      decorationColor:
-                                          Theme.of(context).colorScheme.primary,
-                                    ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+      body: FormLayout(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Create Account',
+                style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
                     ),
-                  ),
+              ),
+              Text(
+                'Join th green revolution!',
+                style: Theme.of(context).textTheme.labelLarge,
+                textAlign: TextAlign.left,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              KTextField(
+                label: 'First Name',
+                hintText: 'First Name',
+                controller: fNameController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your first name';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              KTextField(
+                controller: lNameController,
+                label: 'Last Name',
+                hintText: 'Last Name',
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your last name';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              KTextField(
+                controller: emailController,
+                label: 'Email',
+                hintText: 'Email Address',
+                validator: (value) {
+                  if (value == null || value.isEmpty || !value.contains('@')) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              // will only use country hence to change to drop down menu
+
+              KTextField(
+                controller: passwordController,
+                label: 'Password',
+                hintText: 'Enter Password',
+                isObscured: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty || value.length < 8) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+
+              PrimaryButton(
+                text: 'Create Account',
+                onPressed: !isLoading ? submit : null,
+                isLoading: isLoading,
+              ),
+
+              const SizedBox(
+                height: 50,
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  text: 'By Creating an account you agree tou our ',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  children: [
+                    TextSpan(
+                      text: 'Terms of Service',
+                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color: Theme.of(context).colorScheme.primary,
+                            decoration: TextDecoration.underline,
+                            decorationColor:
+                                Theme.of(context).colorScheme.primary,
+                          ),
+                    ),
+                    const TextSpan(text: ' and '),
+                    TextSpan(
+                      text: 'Privacy Policy',
+                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color: Theme.of(context).colorScheme.primary,
+                            decoration: TextDecoration.underline,
+                            decorationColor:
+                                Theme.of(context).colorScheme.primary,
+                          ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          );
-        },
+            ],
+          ),
+        ),
       ),
     );
   }
