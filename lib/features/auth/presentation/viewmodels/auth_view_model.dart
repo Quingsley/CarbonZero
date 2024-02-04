ede7413c6dea748dd2dffab2f42a40bf277dee3b
quingsley <jeromejuma25@gmail.com>
2024-02-04 06:45:57 +0300

    Add init() method to AuthViewModel

--- lib/features/auth/presentation/viewmodels/auth_view_model.dart
+++ lib/features/auth/presentation/viewmodels/auth_view_model.dart
@@ -2,10 +2,16 @@ import 'dart:async';
 
 import 'package:carbon_zero/features/auth/data/models/user_model.dart';
 import 'package:carbon_zero/features/auth/data/repositories/auth_repo_impl.dart';
+import 'package:carbon_zero/services/local_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// [AuthViewModel] class is a view model for authentication
 class AuthViewModel extends AsyncNotifier<UserModel?> {
+  /// initializes the user
+  Future<void> init() async {
+    state = await AsyncValue.guard(() => LocalStorage().getUser());
+  }
+
   /// sign up method called by the view
   /// to sign up a user
   Future<void> signUp(String password, UserModel user) async {
@@ -22,10 +28,12 @@ class AuthViewModel extends AsyncNotifier<UserModel?> {
   }
 
   /// upload profile image method called by the view
-  Future<void> uploadProfileImage(String path, String userId) async {
+  Future<void> uploadProfileImage(String path) async {
     final repo = ref.read(authRepositoryProvider);
     state = const AsyncLoading();
-    state = await AsyncValue.guard(() => repo.uploadProfileImage(path, userId));
+    state = await AsyncValue.guard(
+      () => repo.uploadProfileImage(path, state.value!.userId!),
+    );
   }
 
   /// forget password method
@@ -42,8 +50,16 @@ class AuthViewModel extends AsyncNotifier<UserModel?> {
     state = await AsyncValue.guard(repo.signOut);
   }
 
+  /// update user details
+  Future<void> updateUser(UserModel user) async {
+    final repo = ref.read(authRepositoryProvider);
+    state = const AsyncLoading();
+    state = await AsyncValue.guard(() => repo.updateUser(user));
+  }
+
   @override
-  FutureOr<UserModel?> build() {
+  FutureOr<UserModel?> build() async {
+    await init();
     return state.value;
   }
 }
