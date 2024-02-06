cbd2aff9e6ed9b47587d8fa4bd9b52e3ddece29e
quingsley <jeromejuma25@gmail.com>
2024-02-06 08:03:21 +0300

    Refactor dbProvider to return null if user is not authenticated

--- lib/core/providers/shared_providers.dart
+++ lib/core/providers/shared_providers.dart
@@ -4,8 +4,12 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// will provide an instance of [FirebaseFirestore]
-final dbProvider =
-    Provider<FirebaseFirestore>((ref) => FirebaseFirestore.instance);
+final dbProvider = Provider<FirebaseFirestore?>((ref) {
+  final authStateChanges = ref.watch(authStateChangesProvider);
+  return authStateChanges.value?.uid != null
+      ? FirebaseFirestore.instance
+      : null;
+});
 
 /// will provide an instance of [FirebaseAuth]
 final authInstanceProvider =
@@ -16,7 +20,7 @@ final storageProvider =
     Provider<FirebaseStorage>((ref) => FirebaseStorage.instance);
 
 /// will listen to auth state changes
-final authStateChanges = Provider<Stream<User?>>(
+final authStateChangesProvider = StreamProvider<User?>(
   (ref) {
     final auth = ref.watch(authInstanceProvider);
     return auth.authStateChanges();
