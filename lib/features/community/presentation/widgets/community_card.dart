ee75768d09fba31108ab339f810b518c06962f68
quingsley <jeromejuma25@gmail.com>
2024-02-08 05:23:05 +0300

    Refactor CommunityCard widget to use CommunityModel

--- lib/features/community/presentation/widgets/community_card.dart
+++ lib/features/community/presentation/widgets/community_card.dart
@@ -1,4 +1,5 @@
 import 'package:carbon_zero/core/extensions.dart';
+import 'package:carbon_zero/features/community/data/models/community_model.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -7,28 +8,12 @@ import 'package:go_router/go_router.dart';
 class CommunityCard extends StatelessWidget {
   /// constructor call
   const CommunityCard({
-    required this.image,
-    required this.name,
-    required this.members,
-    required this.tags,
-    this.description,
+    required this.community,
     super.key,
   });
 
-  /// profile picture of the community
-  final String image;
-
-  /// name of the community
-  final String name;
-
-  /// description of the community
-  final String? description;
-
-  /// number of members in the community
-  final int members;
-
-  /// tags of the community
-  final List<String> tags;
+  /// community model
+  final CommunityModel community;
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +22,8 @@ class CommunityCard extends StatelessWidget {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
         image: DecorationImage(
-          image: AssetImage(image),
+          onError: (exception, stackTrace) => const SizedBox(),
+          image: NetworkImage(community.posterId),
           fit: BoxFit.cover,
         ),
         color: context.colors.primaryContainer,
@@ -58,22 +44,21 @@ class CommunityCard extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  name,
+                  community.name,
                   style: context.textTheme.titleMedium?.copyWith(
                     color: context.colors.onPrimary,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                if (description != null) const SizedBox(height: 4),
-                if (description != null)
-                  Text(
-                    description!,
-                    style: context.textTheme.titleSmall
-                        ?.copyWith(color: context.colors.onPrimary),
-                  ),
+                const SizedBox(height: 4),
+                Text(
+                  community.description,
+                  style: context.textTheme.titleSmall
+                      ?.copyWith(color: context.colors.onPrimary),
+                ),
                 const SizedBox(width: 6),
                 Text(
-                  tags.join(', '),
+                  community.tags.join(', '),
                   style: context.textTheme.labelSmall
                       ?.copyWith(color: context.colors.onPrimary),
                 ),
@@ -88,13 +73,14 @@ class CommunityCard extends StatelessWidget {
                     ),
                     const SizedBox(width: 4),
                     Text(
-                      '$members members',
+                      '${community.members} ${community.members == 1 ? "member" : "members"}',
                       style: context.textTheme.titleSmall
                           ?.copyWith(color: context.colors.onPrimary),
                     ),
                     const Spacer(),
                     ElevatedButton(
-                      onPressed: () => context.go('/community/details'),
+                      onPressed: () =>
+                          context.go('/community/details', extra: community),
                       style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(12),
