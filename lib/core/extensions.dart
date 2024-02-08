2776079009b290c6962ea8366d888060405523f1
quingsley <jeromejuma25@gmail.com>
2024-02-08 05:20:08 +0300

    Add model converters to CollectionReference

--- lib/core/extensions.dart
+++ lib/core/extensions.dart
@@ -1,3 +1,6 @@
+import 'package:carbon_zero/features/auth/data/models/user_model.dart';
+import 'package:carbon_zero/features/community/data/models/community_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 /// [AppThemeExtension] extension will get the current textTheme of the app
@@ -7,4 +10,24 @@ extension AppThemeExtension on BuildContext {
 
   /// gets the app current [ColorScheme]
   ColorScheme get colors => Theme.of(this).colorScheme;
+}
+
+/// [CollectionReference] extension will add a community model converter
+/// to the collection reference
+extension ModelConverter on CollectionReference {
+  /// adds a community model converter to the collection reference
+  CollectionReference<CommunityModel> withCommunityModelConverter() {
+    return withConverter<CommunityModel>(
+      fromFirestore: (snapshot, _) => CommunityModel.fromJson(snapshot.data()!),
+      toFirestore: (community, _) => community.toJson(),
+    );
+  }
+
+  /// [CollectionReference] extension will add a user model converter
+  CollectionReference<UserModel> withUserModelConverter() {
+    return withConverter<UserModel>(
+      fromFirestore: (snapshot, _) => UserModel.fromJson(snapshot.data()!),
+      toFirestore: (user, _) => user.toJson(),
+    );
+  }
 }
