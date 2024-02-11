ae02eafe677a6fd752dd730a600c95dae3a1eb25
quingsley <jeromejuma25@gmail.com>
2024-02-11 04:01:51 +0300

    Add onboarding read in main() function

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await LocalStorage().init();
+  await LocalStorage().readOnboarding();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
