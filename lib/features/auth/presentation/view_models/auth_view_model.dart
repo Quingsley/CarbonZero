483004ba76036dda1ad3d5d4b16cd43202c0b00e
quingsley <jeromejuma25@gmail.com>
2024-02-13 07:58:43 +0300

    Commented out unused code in AuthViewModel

--- lib/features/auth/presentation/view_models/auth_view_model.dart
+++ lib/features/auth/presentation/view_models/auth_view_model.dart
@@ -3,15 +3,15 @@ import 'dart:async';
 import 'package:carbon_zero/core/providers/shared_providers.dart';
 import 'package:carbon_zero/features/auth/data/models/user_model.dart';
 import 'package:carbon_zero/features/auth/data/repositories/auth_repo_impl.dart';
-import 'package:carbon_zero/services/local_storage.dart';
+// import 'package:carbon_zero/services/local_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// [AuthViewModel] class is a view model for authentication
 class AuthViewModel extends AsyncNotifier<void> {
-  /// initializes the user
-  Future<void> init() async {
-    state = await AsyncValue.guard(() => LocalStorage().getUser());
-  }
+  // /// initializes the user
+  // Future<void> init() async {
+  //   state = await AsyncValue.guard(() => LocalStorage().getUser());
+  // }
 
   /// sign up method called by the view
   /// to sign up a user
@@ -61,7 +61,7 @@ class AuthViewModel extends AsyncNotifier<void> {
 
   @override
   FutureOr<void> build() async {
-    await init();
+    // await init();
   }
 }
 
