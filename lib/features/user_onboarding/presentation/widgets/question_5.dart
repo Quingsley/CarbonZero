852058fc81824b560cb88c9bb93c2d2f164aedc4
quingsley <jeromejuma25@gmail.com>
2024-02-15 07:53:58 +0300

    Add energy consumption slider and display number of people in household

--- lib/features/user_onboarding/presentation/widgets/question_5.dart
+++ lib/features/user_onboarding/presentation/widgets/question_5.dart
@@ -1,11 +1,12 @@
 import 'package:carbon_zero/core/constants/constants.dart';
 import 'package:carbon_zero/core/extensions.dart';
 import 'package:carbon_zero/core/widgets/primary_button.dart';
+import 'package:carbon_zero/features/user_onboarding/presentation/widgets/box_image.dart';
 import 'package:carbon_zero/features/user_onboarding/presentation/widgets/footer_reference.dart';
 import 'package:carbon_zero/features/user_onboarding/providers/user_onboarding_providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
+// import 'package:go_router/go_router.dart';
 
 /// Energy consumption question
 class EnergyConsumptionQ extends ConsumerWidget {
@@ -17,16 +18,40 @@ class EnergyConsumptionQ extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final numOfPeople = ref.watch(numOfPeopleProvider);
     return Column(
       children: [
         Text(
-          'Tell us about your household energy consumption',
+          'Tell us about your household energy consumption for the last 12 months.',
           style: context.textTheme.titleLarge,
           textAlign: TextAlign.center,
         ),
-        const SizedBox(height: 40),
+        const SizedBox(height: 20),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Text(
+                'How many people live in your household?',
+                style: context.textTheme.titleSmall,
+                textAlign: TextAlign.start,
+              ),
+            ),
+            Expanded(flex: 2, child: Chip(label: Text(numOfPeople.toString()))),
+          ],
+        ),
+        Slider(
+          value: numOfPeople.toDouble(),
+          min: 1,
+          divisions: 49,
+          max: 50,
+          onChanged: (val) {
+            ref.read(numOfPeopleProvider.notifier).state = val.toInt();
+          },
+        ),
         Expanded(
-          flex: 5,
+          flex: 8,
           child: Material(
             borderRadius: BorderRadius.circular(10),
             elevation: 3,
@@ -35,15 +60,16 @@ class EnergyConsumptionQ extends ConsumerWidget {
                 (e) {
                   final unit = e == EnergyConsumptionType.wood
                       ? 'Tonnes'
-                      : e == EnergyConsumptionType.lpg
-                          ? 'Litres'
-                          : 'KWh';
+                      : e == EnergyConsumptionType.lpg ||
+                              e == EnergyConsumptionType.gas
+                          ? 'Kg'
+                          : e == EnergyConsumptionType.heatingOil
+                              ? 'litres'
+                              : 'KWh';
                   return Padding(
                     padding: const EdgeInsets.only(bottom: 4),
                     child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: AssetImage(getEnergyAsset(e)),
-                      ),
+                      leading: BoxImage(path: getEnergyAsset(e)),
                       title: Text(e.label),
                       trailing: SizedBox(
                         width: 120,
@@ -72,7 +98,7 @@ class EnergyConsumptionQ extends ConsumerWidget {
                                         .read(
                                           energyConsumptionProvider.notifier,
                                         )
-                                        .state[e] = int.parse(value);
+                                        .state[e] = double.parse(value);
                                   }
                                 },
                               ),
@@ -100,9 +126,12 @@ class EnergyConsumptionQ extends ConsumerWidget {
         const Spacer(),
         FooterReference(onTap: () {}),
         PrimaryButton(
-          text: 'Finish',
+          text: 'Continue',
           onPressed: () {
-            context.go('/carbon-footprint-results');
+            controller.nextPage(
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInOut,
+            );
           },
         ),
         const SizedBox(height: 20),
