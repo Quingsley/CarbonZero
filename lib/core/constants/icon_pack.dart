8aff8c1ac7445a1443789ea64326aa2188d38c5f
quingsley <jeromejuma25@gmail.com>
2024-02-22 18:12:39 +0300

    Add eco-friendly icons to icon pack

---
+++ lib/core/constants/icon_pack.dart
@@ -0,0 +1,185 @@
+/// eco friendly icons
+final List<String> ecoFriendlyIcons = [
+  ...foodIcons,
+  ...beverageIcons,
+  ...sportsIcons,
+  ...transportIcons,
+  ...homeIcons,
+  ...natureIcons,
+  ...energyIcons,
+  ...communityIcons,
+  ...healthIcons,
+  ...financialIcons,
+  ...miscellaneousIcons,
+];
+
+/// Food Icons
+List<String> foodIcons = [
+  '🥦',
+  '🍏',
+  '🥕',
+  '🥑',
+  '🥬',
+  '🍅',
+  '🥒',
+  '🌽',
+  '🍇',
+  '🍓',
+  '🍊',
+  '🥥',
+  '🍌',
+];
+
+/// Beverage Icons
+List<String> beverageIcons = [
+  '☕',
+  '🍵',
+  '🥤',
+  '🥛',
+  '🍹',
+  '🥂',
+  '🍺',
+  '🍷',
+];
+
+/// Sports Icons
+List<String> sportsIcons = [
+  '🚴',
+  '🏃‍♂️',
+  '🏊‍♂️',
+  '🧘‍♀️',
+  '⛹️‍♀️',
+  '🚣‍♂️',
+  '🏄‍♂️',
+  '🏋️‍♂️',
+  '🧗‍♂️',
+  '🏌️‍♀️',
+  '🏇',
+  '🏂',
+];
+
+/// Transport Icons
+List<String> transportIcons = [
+  '🚶‍♂️',
+  '🚲',
+  '🚍',
+  '🚆',
+  '🚊',
+  '🚗',
+  '🚘',
+  '🚞',
+  '🚠',
+  '🚡',
+  '🚲',
+  '🚎',
+  '🚐',
+];
+
+/// Home Icons
+List<String> homeIcons = [
+  '🏡',
+  '💡',
+  '🌱',
+  '♻️',
+  '🌞',
+  '🚰',
+  '🌳',
+  '🛠️',
+  '🌍',
+  '🌊',
+];
+
+/// Nature and Environment Icons
+List<String> natureIcons = [
+  '🌳',
+  '🌿',
+  '🌻',
+  '🍃',
+  '🌲',
+  '🌊',
+  '🏞️',
+  '🏔️',
+  '🌍',
+  '🌱',
+];
+
+/// Energy and Technology Icons
+List<String> energyIcons = [
+  '💡',
+  '⚡',
+  '🌞',
+  '🌬️',
+  '🌊',
+  '🌧️',
+  '🌈',
+  '🚀',
+  '🛰️',
+  '🔋',
+];
+
+/// Community and Activism Icons
+List<String> communityIcons = [
+  '🤝',
+  '🗣️',
+  '📢',
+  '📣',
+  '🌐',
+  '🎗️',
+  '🌟',
+  '🎈',
+  '🏆',
+];
+
+/// Health and Wellness Icons
+List<String> healthIcons = [
+  '🍎',
+  '🥗',
+  '🏋️‍♂️',
+  '🧘‍♀️',
+  '🚴‍♀️',
+  '🏃‍♀️',
+  '🛌',
+  '💤',
+  '🚰',
+  '🚿',
+];
+
+/// Financial Icons
+List<String> financialIcons = [
+  '💰',
+  '💵',
+  '💳',
+  '💸',
+  '📈',
+  '📉',
+  '💹',
+  '💱',
+  '🏦',
+  '🏧',
+];
+
+/// Miscellaneous Icons
+List<String> miscellaneousIcons = [
+  '🎉',
+  '💼',
+  '📅',
+  '✅',
+  '📝',
+  '📌',
+  '🔍',
+  '💡',
+  '🔧',
+  '📷',
+  '📱',
+  '💻',
+  '📚',
+  '🔒',
+  '❤️',
+  '🌈',
+  '☀️',
+  '🌧️',
+  '❄️',
+  '⭐',
+  '🎨',
+  '🔔',
+];
