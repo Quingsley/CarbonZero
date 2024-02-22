85b2968ff82181dd0d183ee65e9a78cdbf07071b
quingsley <jeromejuma25@gmail.com>
2024-02-22 18:21:12 +0300

    Add co2Emitted, color, and points properties to ActivityTile

--- lib/features/home/presentation/widgets/activity_tile.dart
+++ lib/features/home/presentation/widgets/activity_tile.dart
@@ -1,3 +1,4 @@
+import 'package:carbon_zero/core/extensions.dart';
 import 'package:flutter/material.dart';
 
 /// [ActivityTile] widget of the app.
@@ -5,39 +6,59 @@ class ActivityTile extends StatelessWidget {
   /// Create const instance of [ActivityTile] widget.
   const ActivityTile({
     required this.title,
-    required this.subtitle,
+    required this.co2Emitted,
     required this.icon,
+    required this.color,
+    required this.points,
     super.key,
-    this.trailing,
   });
 
   /// Activity title.
   final String title;
 
-  /// Activity subtitle.
-  final String subtitle;
-
-  /// Activity trailing.
-  final String? trailing;
+  /// Activity co2Emitted.
+  final String co2Emitted;
 
   /// Activity icon.
-  final IconData icon;
+  final String icon;
+
+  /// color of activity
+  final int color;
+
+  /// carbon points earned
+  final int points;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Icon(icon),
+      leading: SizedBox(
+        height: 30,
+        width: 30,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: ColoredBox(
+            color: Color(color),
+            child: Center(
+              child: Text(
+                icon,
+                style: context.textTheme.headlineSmall,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ),
+      ),
       title: Text(title),
       subtitle: Text(
-        subtitle,
+        '$points carbon points',
         style: TextStyle(color: Colors.grey[400]),
       ),
       trailing: Text.rich(
         TextSpan(
-          text: '-500gr ',
+          text: co2Emitted,
           children: [
             TextSpan(
-              text: 'Co2',
+              text: 'g CO2e',
               style: TextStyle(color: Colors.grey[500]),
             ),
           ],
