b5e45f41820162ddd48bdc7f7d5db09bae61c3fb
quingsley <jeromejuma25@gmail.com>
2024-02-22 18:14:29 +0300

    Add ActivityRepository class and activityRepositoryProvider

---
+++ lib/features/activities/data/repositories/activity_repository.dart
@@ -0,0 +1,33 @@
+import 'package:carbon_zero/features/activities/data/models/activity_model.dart';
+import 'package:carbon_zero/features/activities/data/models/activity_recording_model.dart';
+import 'package:carbon_zero/features/activities/data/remote_data_source/activity_data_source.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// ActivityRepository that reads from the remote data source
+class ActivityRepository implements IActivityDataSource {
+  /// [ActivityRepository] class constructor
+  ActivityRepository({required ActivityDataSource dataSource})
+      : _dataSource = dataSource;
+
+  final ActivityDataSource _dataSource;
+  @override
+  Future<void> createActivity(ActivityModel activity) async {
+    await _dataSource.createActivity(activity);
+  }
+
+  @override
+  Stream<List<ActivityModel>> getActivities(String parentId) {
+    return _dataSource.getActivities(parentId);
+  }
+
+  @override
+  Future<void> recordActivity(ActivityRecordingModel activity) async {
+    await _dataSource.recordActivity(activity);
+  }
+}
+
+/// activity repo provider
+final activityRepositoryProvider = Provider<ActivityRepository>((ref) {
+  final dataSource = ref.read(activityDataSourceProvider);
+  return ActivityRepository(dataSource: dataSource);
+});
