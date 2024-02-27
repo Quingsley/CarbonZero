0640598268532b9a2e9926b9fc5d42fc0bbe5f49
quingsley <jeromejuma25@gmail.com>
2024-02-27 22:03:12 +0300

    Refactor RewardProgressCard widget and add Join Challenge button

--- lib/features/rewards/presentation/widgets/reward_progress_card.dart
+++ lib/features/rewards/presentation/widgets/reward_progress_card.dart
@@ -1,12 +1,16 @@
 import 'package:carbon_zero/core/extensions.dart';
+import 'package:carbon_zero/core/widgets/primary_button.dart';
 import 'package:carbon_zero/features/activities/data/models/activity_model.dart';
+import 'package:carbon_zero/features/activities/presentation/view_models/activity_view_model.dart';
+import 'package:carbon_zero/features/auth/presentation/view_models/auth_view_model.dart';
 import 'package:firebase_cached_image/firebase_cached_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 
 /// card used to show the progress of the user in the rewards section
-class RewardProgressCard extends StatelessWidget {
+class RewardProgressCard extends ConsumerWidget {
   /// constructor call
   const RewardProgressCard({
     required this.activityModel,
@@ -17,7 +21,11 @@ class RewardProgressCard extends StatelessWidget {
   final ActivityModel activityModel;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userStreamProvider);
+    final hasJoined = activityModel.participants.contains(user.value?.userId);
+    final activityVm = ref.watch(activityViewModelProvider);
+    final isLoading = activityVm is AsyncLoading;
     return ClipRRect(
       borderRadius: BorderRadius.circular(12),
       child: InkWell(
@@ -72,31 +80,47 @@ class RewardProgressCard extends StatelessWidget {
                   const SizedBox(
                     height: 12,
                   ),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      SizedBox(
-                        width: 170,
-                        child: LinearProgressIndicator(
-                          value: activityModel.progress,
-                          backgroundColor: Colors.grey.withOpacity(.62),
-                          valueColor: AlwaysStoppedAnimation<Color>(
-                            Color(activityModel.color),
+                  if (hasJoined)
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SizedBox(
+                          width: 170,
+                          child: LinearProgressIndicator(
+                            value: activityModel.progress,
+                            backgroundColor: Colors.grey.withOpacity(.62),
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                              Color(activityModel.color),
+                            ),
+                            color: Theme.of(context).colorScheme.primary,
+                            borderRadius: BorderRadius.circular(12),
+                            minHeight: 8,
                           ),
-                          color: Theme.of(context).colorScheme.primary,
-                          borderRadius: BorderRadius.circular(12),
-                          minHeight: 8,
                         ),
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      Text(
-                        '${(activityModel.progress * 100).toStringAsFixed(2)}%',
-                      ),
-                    ],
-                  ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          '${(activityModel.progress * 100).toStringAsFixed(2)}%',
+                        ),
+                      ],
+                    ),
+                  if (!hasJoined)
+                    PrimaryButton(
+                      text: 'Join Challenge',
+                      isLoading: isLoading,
+                      onPressed: isLoading
+                          ? null
+                          : () {
+                              ref
+                                  .read(activityViewModelProvider.notifier)
+                                  .addParticipant(
+                                    activityModel.id!,
+                                    user.value!.userId!,
+                                  );
+                            },
+                    ),
                 ],
               ),
             ],
