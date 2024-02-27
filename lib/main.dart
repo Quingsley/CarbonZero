314f3015cd88d14b34ee7d730650cb8e1fe47ea8
quingsley <jeromejuma25@gmail.com>
2024-02-27 21:58:12 +0300

    Add Firebase Messaging background handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:carbon_zero/app.dart';
+import 'package:carbon_zero/core/constants/constants.dart';
 import 'package:carbon_zero/core/providers/shared_providers.dart';
 import 'package:carbon_zero/core/theme/theme.dart';
 import 'package:carbon_zero/features/splash/presentation/pages/splash_screen.dart';
@@ -7,9 +10,32 @@ import 'package:carbon_zero/firebase_options.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  final storage = await SharedPreferences.getInstance();
+  if (kDebugMode) {
+    print('Handling a background message: ${message.messageId}');
+    print('Message data: ${message.data}');
+    print('Message notification: ${message.notification?.title}');
+    print('Message notification: ${message.notification?.body}');
+  }
+  // fix should be stored in local storage
+  final dataStored = storage.get(notificationKey);
+  if (dataStored != null) {
+    final pendingNotifications =
+        jsonDecode(dataStored.toString()) as List<RemoteMessage>..add(message);
+    await storage.setString(notificationKey, jsonEncode(pendingNotifications));
+  } else {
+    final initialList = <RemoteMessage>[message];
+    await storage.setString(notificationKey, jsonEncode(initialList));
+  }
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,6 +55,8 @@ Future<void> main() async {
     FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
     return true;
   };
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await AwesomeNotifications().initialize(
     // set the icon to null if you want to use the default app icon
     null,
