cac6ecef45e93a2883fd415281cd050d8bea9d9e
quingsley <jeromejuma25@gmail.com>
2024-03-04 20:23:21 +0300

    Add currentTabProvider to food_emission_providers.dart

--- lib/features/emission/presentation/providers/food_emission_providers.dart
+++ lib/features/emission/presentation/providers/food_emission_providers.dart
@@ -4,3 +4,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final mealCountProvider = StateProvider<int>((ref) {
   return 1;
 });
+
+/// current tab provider
+final currentTabProvider = StateProvider<int>((ref) {
+  return 0;
+});
